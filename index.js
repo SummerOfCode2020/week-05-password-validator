af6d7e4d2f5f97d453da003fc87ff31825bf6400
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-07-04 14:30:42 -0400

    Write code for validatePassword f'n to pass test for when password is too short

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Do work!
+/* Do work! Below are the requirements for the Password Validator from the README for quick reference
+Your task is to create a `validatePassword` function which will return `true` when the password meets all requirements.
+
+- minimum of 8 characters
+
+Must contain at least one
+
+- lower case letter
+- upper case letter
+- numeric value
+- special character */
+
+module.exports = function validatePassword(password) {
+  for (let initialChar = 0; initialChar < password.length; initialChar++) {
+    if (password.length >= 8) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
