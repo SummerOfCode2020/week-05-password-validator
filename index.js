e5c06a66c2f48d0b8287ee787b01c4267e146e57
Sharon Lin <hsiang.lin@gmail.com>
2020-07-04 00:34:14 -0400

    Adding two more rules to include oneUpper and one LowerCase

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
-const lowerCase= []
-
 
 function validatePassword(password) {
+  let eightChar = true
+  let oneUpperCase = true
+  let finalValidation = true
+  let oneLowerCase = true
+  // let numVal = true
+  // let specChar = true
 
-  let validation = true
-
-  
-  
   if (password.length >= 8) {
-
-    validation = true
+    eightChar = true
   }
 
   else {
-    validation = false
+    eightChar = false
   }
 
-  return validation
+  const arrPassword = password.split()
 
+  arrPassword.forEach((char) => {
+    if (char == char.toUpperCase()) {
+      oneUpperCase = true
+    }
+    else {
+      oneUpperCase = false
+    }
+  })
+
+  arrPassword.forEach((char) => {
+    if (char == char.toLowerCase()) {
+      oneLowerCase = true
+    }
+    else {
+      oneLowerCase = false
+    }
+  })
+
+  if (eightChar == true && oneUpperCase == true && oneLowerCase == true) {
+    finalValidation = true
+  }
+
+  else {
+   finalValidation = false
+  }
+  return finalValidation
 }
 
-
-module.exports = validatePassword;
+module.exports = validatePassword
 
