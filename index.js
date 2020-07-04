1075a9ae9c9df52e70e3d16b7f1ea669a4548d53
Sharon Lin <hsiang.lin@gmail.com>
2020-07-03 23:58:06 -0400

    Wrote validatePassword function, at least minimum 8 character rule passed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Do work!
+const lowerCase= []
+
+
+function validatePassword(password) {
+
+  let validation = true
+
+  
+  
+  if (password.length >= 8) {
+
+    validation = true
+  }
+
+  else {
+    validation = false
+  }
+
+  return validation
+
+}
+
+
+module.exports = validatePassword;
+
