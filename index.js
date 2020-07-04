bdeee76aa5efc30e9005113b1b867ff68d94113c
Sharon Lin <hsiang.lin@gmail.com>
2020-07-04 17:40:09 -0400

    Added at least one number criteria, returned false with the string frorm test

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function validatePassword(password) {
   let eightChar = true
   let oneUpperCase = true
   let finalValidation = true
-  // let oneLowerCase = true
-  // let numVal = true
+  let oneLowerCase = true
+  let numVal = true
   // let specChar = true
 
   if (password.length >= 8) {
@@ -29,23 +29,37 @@ function validatePassword(password) {
       oneUpperCase = false
     }
   }
-  
-  // let lowerCase = []
 
-  // for (let i = 0; i < arrPassword.length; i++) {
-  //   if (arrPassword[i] === arrPassword[i].toLowerCase()) {
-  //     lowerCase.push(arrPassword[i])
-  //   }
-  //   if (lowerCase.length >= 0) {
-  //     oneUpperCase = true
-  //   }
-  //   if (lowerCase.length < 1) {
-  //     oneUpperCase = false
-  //   }
-  // }
-  //&&oneLowerCase == true && numVal == true && specChar == true
+  let lowerCase = []
 
-  if (eightChar === true && oneUpperCase === true ) {
+  for (let i = 0; i < arrPassword.length; i++) {
+    if (arrPassword[i] === arrPassword[i].toLowerCase()) {
+      lowerCase.push(arrPassword[i])
+    }
+    if (lowerCase.length >= 0) {
+      oneLowerCase = true
+    }
+    if (lowerCase.length < 1) {
+      oneLowerCase = false
+    }
+  }
+
+  let oneNumber = []
+  for (let i = 0; i < arrPassword.length; i++) {
+    if (arrPassword[i] === Number.isInteger(arrPassword[i])) {
+      oneNumber.push(arrPassword[i])
+    }
+    if (oneNumber.length >= 0) {
+      numVal = true
+    }
+    if (oneNumber.length < 1) {
+      numVal = false
+    }
+  }
+
+  //  && specChar == true
+
+  if (eightChar === true && oneUpperCase === true && lowerCase === true && numVal === true) {
 
     finalValidation = true
   }
@@ -56,6 +70,7 @@ function validatePassword(password) {
   return finalValidation
 
 }
-console.log(validatePassword('p455w0Rd'))
+console.log("no uppercase", validatePassword('p455w0Rd'))
+console.log("no number", validatePassword('Password!'))
 module.exports = validatePassword;
 
