a0b2a8995922a11651d75f53664b8fd2ffdaae1b
Colleen Kingsley <colleen.kingsley89@gmail.com>
2020-07-06 09:28:38 -0400

    Use of regexp solution

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Do work!
+const { expect } = require('chai')
+
+// Function returns true when all requirements are met
+module.exports = function validatePassword(password) {
+  // password has lowercase letters //
+  if (password.match(/[a-z]/) &&
+  // password has uppercase letters //
+        password.match(/[A-Z]/) &&
+  // password has numerical values //
+        password.match(/[0-9]/) &&
+  // password has special characters //
+      password.match(/[!@#$%^&*()]/) &&
+  // password has minimum of 8 characters //
+        password.length >= 8) {
+    password = true
+  } else {
+    password = false
+  }
+
+  return password
+}
+
+
+
