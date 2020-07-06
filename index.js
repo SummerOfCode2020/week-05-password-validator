46cb384d3def66e2acca92fe14011ac855cb02f2
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-07-05 20:26:07 -0400

    Tweak and update code to pass all validation tests

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
-/* Do work! Below are the requirements for the Password Validator from the README for quick reference
-Your task is to create a `validatePassword` function which will return `true` when the password meets all requirements.
-
-- minimum of 8 characters
-
-Must contain at least one
-
-- lower case letter
-- upper case letter
-- numeric value
-- special character */
-
-module.exports = function validatePassword(password) {
-  for (let initialChar = 0; initialChar < password.length; initialChar++) {
-    if (password.length >= 8) {
-      return true
-    } else {
-      return false
-    }
+/* eslint-disable no-console */
+function validatePassword(password) {
+  if (password.length >= 8 && characterValidation(password)) {
+    return true
+  } else {
+    return false
   }
 }
+
+// Create a function to test each password as an array and loop through each item.
+
+function characterValidation(password) {
+  // Create const variables to compare the elements in the password to:
+  // eslint-disable-next-line max-len
+  const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  // eslint-disable-next-line max-len
+  const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  // eslint-disable-next-line max-len
+  const specialCharacters = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '^', '_', '`', '{', '|', '}', '\\', '~']
+
+  // set all rules to false to start; perform tests to see if they are true
+  let upperCaseCheck = false
+  let lowerCaseCheck = false
+  let numericCheck = false
+  let specialCharCheck = false
+
+  // split the password into an array to perform a loop to search for each rule
+  const characterArray = password.split('')
+
+  // check the password for uppercase letters
+  characterArray.forEach((character) => {
+    if (upperCase.includes(character)) {
+      upperCaseCheck = true
+    }
+
+    // check the password for lowercase letters
+    if (lowerCase.includes(character)) {
+      lowerCaseCheck = true
+    }
+
+    // check for numeric value
+    if (!isNaN(parseInt(character))) {
+      numericCheck = true
+    }
+
+    // check for special characters
+    if (specialCharacters.includes(character)) {
+      specialCharCheck = true
+    }
+  })
+  // this will see if each test together is true, then return true for the entire character validation.
+
+  return (upperCaseCheck && lowerCaseCheck && numericCheck && specialCharCheck)
+}
+
+module.exports = validatePassword
+
+// testing
+console.log(validatePassword('P455w0rd!'))
+console.log(validatePassword('P4w0rd!'))
+console.log(validatePassword('P455W0RD!'))
+console.log(validatePassword('p455w0rd!'))
+console.log(validatePassword('Password!'))
+console.log(validatePassword('P455w0rd1'))
