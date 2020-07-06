4ca8f4de2c61baa5f46483de4ea8a60d8317f7cb
Alvin <fulmar38251@letterboxes.org>
2020-07-06 05:16:35 -0400

    Modify validatePassword function with lower case check

--- index.js
+++ index.js
@@ -2,13 +2,20 @@
 // in Slack, JR instructed us not to use regular expressions
 function validatePassword(pword) {
   const minLength = 8
+  const lower = 'abcdefghijklmnopqrstuvwxyz'
   let lengthCheck = false
+  let lowerCheck = false
 
   if (pword.length >= minLength) {
     lengthCheck = true
   }
 
-  return lengthCheck
-}
+  for (let x = 0; x < pword.length; x++) {
+    if (lower.search(pword[x]) >= 0) {
+      lowerCheck = true
+    }
 
+    return lengthCheck && lowerCheck
+  }
+}
 module.exports = validatePassword
