fc76979d602aaba72c426deeb9fcca0395e9ffb8
Alvin <fulmar38251@letterboxes.org>
2020-07-06 04:22:37 -0400

    Start of validatePassword function with length check

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Do work!
+// in Slack, JR instructed us not to use regular expressions
+function validatePassword(pword) {
+  const minLength = 8
+  let lengthCheck = false
+
+  if (pword.length >= minLength) {
+    lengthCheck = true
+  }
+
+  return lengthCheck
+}
+
+module.exports = validatePassword
