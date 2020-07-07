934050213d7113ec4f06bda794d9051dc315e678
mvrcusj <mvrcusj@gmail.com>
2020-07-07 14:01:48 -0400

    Pass all tests except for the special character requirement test

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Do work!
+/* eslint-disable nonblock-statement-body-position */
+function validatePassword(password) {
+  let allRequirementsMet = false
+  let specCharacters = '!"#$%&/' / '()*+,-.'
+
+  // Determine if the password length is more than or equal to 8 then..
+  if (password.length >= 8) {
+    // Using ASCII index and charCodeAt method (thanks to Jkearns!)
+    // Test further requirements
+    // Test for uppercase characters
+    for (let i = 0; i < password.length; i++) {
+      if (password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90)
+      // Test for lowercase characters
+        for (let i = 0; i < password.length; i++) {
+          if (password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122)
+          // Test for numeric value
+            for (let i = 0; i < password.length; i++) {
+              if (password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57)
+                allRequirementsMet = true
+            }
+        }
+    }
+  }
+
+  return allRequirementsMet
+}
+
+module.exports = validatePassword
