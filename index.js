888c8e98ebe8a300475df51e6ad93b46c77b252d
rock43053 <agm.baf@gmail.com>
2020-07-08 21:16:59 -0400

    HW Fully Completed

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function validatePassword(password) {
+  let isSpecial = false
+  let isNumeric = false
+  const stringOfNums = '0123456789'
+
   if (password.length >= 8) { // if the password length is 8 or more then do code after
     let upperCase = password.toUpperCase()
     let lowerCase = password.toLowerCase()
@@ -7,13 +11,19 @@ function validatePassword(password) {
       return false
     } else {
       for (let index = 0; index < password.length; index++) {
-        if (password.charCodeAt(index) >= 32 && password.charCodeAt(index) <= 47 || password.charCodeAt(index) >= 58 && password.charCodeAt(index) <= 64 || password.charCodeAt(index) >= 91 && password.charCodeAt(index) <= 96 || password.charCodeAt(index) >= 123 && password.charCodeAt(index) <= 126) {
-          return true
+        if (password.charCodeAt(index) >= 33 && password.charCodeAt(index) <= 47 ||
+        password.charCodeAt(index) >= 58 && password.charCodeAt(index) <= 64 ||
+        password.charCodeAt(index) >= 91 && password.charCodeAt(index) <= 96 ||
+        password.charCodeAt(index) >= 123 && password.charCodeAt(index) <= 126) {
+          isSpecial = true
+        }
+        if (stringOfNums.includes(password[index])) {
+          isNumeric = true
         }
       }
     }
-  } else {
-    return false
   }
+
+  return isSpecial && isNumeric
 }
 module.exports = validatePassword
